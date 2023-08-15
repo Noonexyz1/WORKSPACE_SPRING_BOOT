65c015c2c5a8d416b1414350eef2cbdd6843030b
Noonexyz1 <aries-d2github@hotmail.com>
2023-08-15 10:31:16 -0400

    Test Campo invalido implementado

--- Micro-proyecto-tienda/micro-usuario-login/src/test/java/com/diego/service/UserLoginServiceTest.java
+++ Micro-proyecto-tienda/micro-usuario-login/src/test/java/com/diego/service/UserLoginServiceTest.java
@@ -2,9 +2,11 @@ package com.diego.service;
 
 import com.diego.dto.request.UserLoginDTO;
 import com.diego.dto.response.UserLoginRegistedDTO;
+import com.diego.dto.response.UserLoginRegisterDTOError;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 class UserLoginServiceTest {
 
@@ -13,6 +15,44 @@ class UserLoginServiceTest {
     @BeforeEach
     public void startUp(){
         userLoginService = new UserLoginServiceImpl();
+    }
+    @Test
+    void cuandoSiSePudoEncontraElUsuarioParaInciarSesionUSER(){
+        //Ghiven datos de incializacion para probar
+        UserLoginDTO userLoginDTO = UserLoginDTO.builder()
+                .correo("ana@ana.com")
+                .contrasena("123456")
+                .build();
+        //valor del resultado esperado
+        UserLoginRegistedDTO userLoginRegistedFoundDTO = UserLoginRegistedDTO.builder()
+                .userName("Ana")
+                .rol("USER")
+                .build();
+
+        //When
+        UserLoginRegistedDTO userLoginRegistedDTO = userLoginService.logUser(userLoginDTO);
+
+        //Then
+        Assertions.assertThat(userLoginRegistedDTO).isEqualTo(userLoginRegistedFoundDTO);
+    }
+    @Test
+    void cuandoSiSePudoEncontraElUsuarioParaInciarSesionADMIN(){
+        //Ghiven datos de incializacion para probar
+        UserLoginDTO userLoginDTO = UserLoginDTO.builder()
+                .correo("diego@diego.com")
+                .contrasena("123456")
+                .build();
+        //valor del resultado esperado
+        UserLoginRegistedDTO userLoginRegistedFoundDTO = UserLoginRegistedDTO.builder()
+                .userName("Diego")
+                .rol("ADMIN")
+                .build();
+
+        //When
+        UserLoginRegistedDTO userLoginRegistedDTO = userLoginService.logUser(userLoginDTO);
+
+        //Then
+        Assertions.assertThat(userLoginRegistedDTO).isEqualTo(userLoginRegistedFoundDTO);
     }
 
     @Test
@@ -39,7 +79,28 @@ class UserLoginServiceTest {
 
     //Aqui mis test de exceptions
     @Test
-    void cuandoLosCamposNoSonValidosDelLadoController(){}
+    void cuandoLosCamposNoSonValidosDelLadoController(){
+        UserLoginDTO userLoginDTO = UserLoginDTO.builder()
+                .correo("")     //Debo poner algun dato erroneo
+                .contrasena("") //Aqui tambien
+                .build();
+        //valor del resultado esperado
+        UserLoginRegisterDTOError userLoginRegisterDTOError = UserLoginRegisterDTOError.builder()
+                .code("ERROR-TYPE-400")
+                .message("Los campos no son validos")
+                .build();
+
+        //When
+        UserLoginRegistedDTO userLoginRegistedDTO = userLoginService.logUser(userLoginDTO);
+
+        Assertions.assertThatThrownBy(() -> userLoginService.logUser(userLoginDTO))
+                .isInstanceOf(MethodArgumentNotValidException.class)
+                        .hasMessage(userLoginRegisterDTOError.toString());
+
+        //Then
+        Assertions.assertThat(userLoginRegistedDTO).isEqualTo(userLoginRegisterDTOError);
+    }
+    
     @Test
     void cuandoElClienteEnviaUnJsonNoValidoController(){}
     @Test
