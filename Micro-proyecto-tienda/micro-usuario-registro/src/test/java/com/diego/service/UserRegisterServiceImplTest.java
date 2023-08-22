f777396bee7bf5a6568996b4514659e3b92ac7c4
Noonexyz1 <aries-d2github@hotmail.com>
2023-08-22 16:19:21 -0400

    micro-registro, test del Service agregado

--- Micro-proyecto-tienda/micro-usuario-registro/src/test/java/com/diego/service/UserRegisterServiceImplTest.java
+++ Micro-proyecto-tienda/micro-usuario-registro/src/test/java/com/diego/service/UserRegisterServiceImplTest.java
@@ -29,35 +29,48 @@ public class UserRegisterServiceImplTest {
                     //estás "mockeando" esta dependencia
     private UserRegisterRepository userRegisterRepository;
 
+
+    private UserReguisterDTO userReguisterDTO;      //Datos de entrada
+    private UserReguisteredDTO userReguisteredDTO;  //Datos de salida esperada
+
     @BeforeEach
     public void setUp(){
         //Preparando el laboratorio
-        MockitoAnnotations.openMocks(this);
-        userRegisterService = new UserRegisterServiceImpl(userRegisterRepository);
-    }
-
-    @Test
-    public void testRegisterNewUser(){
-        UserReguisterDTO userReguisterDTO = UserReguisterDTO.builder()
+        //MockitoAnnotations.openMocks(this);
+        userReguisterDTO = UserReguisterDTO.builder()
                 .nombre("John")
                 .correo("john@john.com")
                 .contrasena("123456")
                 .role(UserTypeRoles.USER)
                 .build();
+
+        userReguisteredDTO = UserReguisteredDTO.builder()
+                .nombre("John")
+                .correo("john@john.com")
+                .build();
+
         Usuario usuario = Usuario.builder()
                 .nombre(userReguisterDTO.nombre())
                 .correo(userReguisterDTO.correo())
                 .contrasena(userReguisterDTO.contrasena())
+                .rol(userReguisterDTO.role())
                 .build();
+
+        /*No hace falta injectar, porque ya lo tenemos anotado, el hara todo por nosotros*/
+        //userRegisterService = new UserRegisterServiceImpl(userRegisterRepository);
 
         //Mocking Comportamiento
         Mockito.when(userRegisterRepository.save(ArgumentMatchers.any(Usuario.class)))
                 .thenReturn(usuario);
+    }
 
-        UserReguisteredDTO userReguisteredDTO = userRegisterService.registerNewUser(userReguisterDTO);
+    @Test
+    public void testRegisterNewUser(){
+        UserReguisteredDTO userReguisteredDTOResultado = userRegisterService.registerNewUser(userReguisterDTO);
 
-        Assertions.assertEquals("John", userReguisteredDTO.nombre());
-        Assertions.assertEquals("john@john.com", userReguisteredDTO.correo());
+        //Assertions.assertEquals("John", userReguisteredDTO.nombre());
+        //Assertions.assertEquals("john@john.com", userReguisteredDTO.correo());
+        Assertions.assertEquals(userReguisteredDTO, userReguisteredDTOResultado);
 
     }
 
